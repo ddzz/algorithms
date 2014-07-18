7f1bc70596a122f310ce4cc2b94d45699abba7cb
Dariusz <ddzien2@gmail.com>
2014-07-18 09:54:13 -0500

    Quicksort test

---
+++ qs1.py
@@ -0,0 +1,39 @@
+
+counter = 0
+
+def quicksort(array,start,end):
+	global counter
+	
+	if start >= end:
+
+		return 0
+		
+	else:
+		part = partition(array,start,end)
+		left = quicksort(array,start,part)				
+		right = quicksort(array,part+1,end)
+		return counter
+
+
+def partition(array,l,r):
+	global counter
+	counter += r-l-1
+
+	p = array[l]
+	
+	i = l + 1
+
+	for j in range(l+1,r):
+		if array[j] < p:					
+			array[i], array[j] = array[j], array[i]
+			i += 1			
+
+	array[l], array[i-1] = array[i-1], array[l]
+	return i-1
+
+array = []
+for line in open('qs.txt','r').readlines():	
+	array.append(int(line))
+
+print len(array)
+print quicksort(array,0,len(array))
